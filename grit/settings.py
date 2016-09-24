30457adcb186459167b2da6379f1a7ce966cd81f
luismoralesp <luismiguel.mopa@gmail.com>
2016-09-24 11:30:32 -0500

    logo

--- grit/settings.py
+++ grit/settings.py
@@ -58,7 +58,7 @@ EXILE_UI = {
     'index_title': 'Proyect planning',
     'media': {
         'logo': {
-            'dashboard': '/media/piscix_logo/logo-grit.png',
+            'dashboard': '/media/grit_logo/logo-grit.png',
             'page': '/media/grit_logo/isotipo-grit.png',
             'login': '/media/grit_logo/logo-grit.png'
         },
